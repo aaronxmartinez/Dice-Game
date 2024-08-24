5355090016273e823a523c2f20a6c33a36785b55
Aaron <aaronmartinezz019@gmail.com>
2024-08-24 14:33:04 -0500

    created second variable of random number, created variables firstDice and secondDice with the query selected img1 and img2 for their respective dice, set random numbers 1-6 to change the dice image

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 // generating random number
-var randomNumber = Math.floor(Math.random() * 6) + 1;
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 // creating variables for dice
-var firstDice = document.getElementById("first-dice");
-var secondDice = document.getElementById("second-dice");
+var firstDice = document.querySelector(".img1");
+var secondDice = document.querySelector(".img2");
 
-// changing image based on random number
-if (randomNumber === 6){
-    document.querySelector("dice-container.first-dice").setAttribute("img, img/dice4.png");
+// changing image based on random number for first dice
+if (randomNumber1 === 1){
+    firstDice.setAttribute("src", "img/dice1.png");
+} else if (randomNumber1 === 2){
+    firstDice.setAttribute("src", "img/dice2.png");
+} else if (randomNumber1 === 3){
+    firstDice.setAttribute("src", "img/dice3.png");
+} else if (randomNumber1 === 4){
+    firstDice.setAttribute("src", "img/dice4.png");
+} else if (randomNumber1 === 5){
+    firstDice.setAttribute("src", "img/dice5.png");
+} else if (randomNumber1 === 6){
+    firstDice.setAttribute("src", "img/dice6.png");
 }

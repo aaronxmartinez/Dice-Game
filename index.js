677b3465103cc564883813349d35aeafde08e4eb
Aaron <aaronmartinezz019@gmail.com>
2024-08-24 14:07:35 -0500

    added variables firstDice and secondDice to javascript files

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // generating random number
 var randomNumber = Math.floor(Math.random() * 6) + 1;
 
+// creating variables for dice
+var firstDice = document.getElementById("first-dice");
+var secondDice = document.getElementById("second-dice");
+
 // changing image based on random number
 if (randomNumber === 6){
-    document.querySelector("dice").setAttribute("img, img/dice6.png");
+    document.querySelector("dice-container.first-dice").setAttribute("img, img/dice4.png");
 }

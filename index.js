396cfcdf054b2365f7b3a966cd495a9bd6b0f941
Aaron <aaronmartinezz019@gmail.com>
2024-08-24 13:50:16 -0500

    changed dice classes to first-dice for the first dice and second-dice for the second dice, working on changing picutre of the image of dice side based on random number

--- index.js
+++ index.js
@@ -2,3 +2,6 @@
 var randomNumber = Math.floor(Math.random() * 6) + 1;
 
 // changing image based on random number
+if (randomNumber === 6){
+    document.querySelector("dice").setAttribute("img, img/dice6.png");
+}

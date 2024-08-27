c74c681137a2ec474a37b54b60878c057f89a363
Aaron <aaronmartinezz019@gmail.com>
2024-08-26 22:11:24 -0500

    changed location of the declaring a winner section

--- index.js
+++ index.js
@@ -18,6 +18,18 @@ if (firstUserInput > 6 || firstUserInput < 1 || secondUserInput > 6 || secondUse
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
+// declaring a winner and if it's a draw
+if (firstUserInput === randomNumber1 && secondUserInput === randomNumber2){
+    document.querySelector("h1").innerHTML = "Draw";
+    document.querySelector("h1").style.fontSize = "10rem";
+} else if (firstUserInput === randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 1 Wins!!";
+    document.querySelector("h1").style.fontSize = "9rem";
+} else if (secondUserInput === randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 Wins!!"
+    document.querySelector("h1").style.fontSize = "9rem";
+}
+
 // creating variables for dice
 var firstDice = document.querySelector(".img1");
 var secondDice = document.querySelector(".img2");
@@ -52,17 +64,6 @@ if (randomNumber2 === 1){
     secondDice.setAttribute("src", "img/dice6.png");
 }
 
-// declaring a winner and if it's a draw
-if (firstUserInput === randomNumber1 && secondUserInput === randomNumber2){
-    document.querySelector("h1").innerHTML = "Draw";
-    document.querySelector("h1").style.fontSize = "10rem";
-} else if (firstUserInput === randomNumber1){
-    document.querySelector("h1").innerHTML = "Player 1 Wins!!";
-    document.querySelector("h1").style.fontSize = "9rem";
-} else if (secondUserInput === randomNumber2){
-    document.querySelector("h1").innerHTML = "Player 2 Wins!!"
-    document.querySelector("h1").style.fontSize = "9rem";
-}
 
 // seeing if user inputs are numbers or not
 if (isNaN(firstUserInput) || isNaN(secondUserInput)){
